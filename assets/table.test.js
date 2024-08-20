3c61767c8c5c7027b6f962fa3e2f941cbbe32b7b
pkdc <pkdchoy@gmail.com>
2024-08-20 19:19:33 +0100

    added 3 more tests for multiple consecutive completed rows

--- assets/table.test.js
+++ assets/table.test.js
@@ -172,6 +172,47 @@ test('removes 3 consecutive completed rows at the top and shifts rows above down
     expect(newScore).toBe(700);
 });
 
+test('removes 3 consecutive completed rows at the top and shifts rows above down, and add score', () => {
+    const box2 = document.createElement("div");
+    root.append(box2);
+
+    const gameBoard = new gameArea(10, 20);
+    box2.append(gameBoard.generateTable());
+
+    const numLinesToComplete = 4;
+    gameBoard.completeLines(numLinesToComplete, 16)
+
+    const completedLine1 = document.querySelectorAll('.y-16');
+    // console.log("completedLine length: ", completedLine.length);
+    completedLine1.forEach(el => {
+        expect(el.classList.contains("occupied")).toBe(true);
+    });
+    const completedLine2 = document.querySelectorAll('.y-17');
+    // console.log("completedLine length: ", completedLine.length);
+    completedLine2.forEach(el => {
+        expect(el.classList.contains("occupied")).toBe(true);
+    });
+    const completedLine3 = document.querySelectorAll('.y-18');
+    // console.log("completedLine length: ", completedLine.length);
+    completedLine3.forEach(el => {
+        expect(el.classList.contains("occupied")).toBe(true);
+    });
+    const completedLine4 = document.querySelectorAll('.y-19');
+    completedLine4.forEach(el => {
+        expect(el.classList.contains("occupied")).toBe(true);
+    });
+
+    // console.log("gameBoard before removal: ", box2.innerHTML);
+
+    const score = 0;
+    const newScore = gameBoard.removeCompletedLines(score);
+
+    // console.log("gameBoard after removal: ", box2.innerHTML);
+
+    // single line completed
+    expect(newScore).toBe(1500);
+});
+
 // test('removes 2 consecutive completed row and shifts rows above down, and add score', () => {
 //     const box2 = document.createElement("div");
 //     root.append(box2);
